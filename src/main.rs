d9c669ce7958ce3f933cfc66460918a142fc0f1e
JiquanWang99 <951873902@qq.com>
2022-01-26 23:51:02 +0800

    feat: formate time

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use chrono::format;
+use chrono::prelude::*;
+use colored::*;
 use exitfailure::ExitFailure;
 use reqwest::Url;
 use serde_derive::{Deserialize, Serialize};
+use std::time::{Duration, UNIX_EPOCH};
 use structopt::StructOpt;
-
-// http://api.openweathermap.org/data/2.5/weather?q=Beijing&APPID=c6c7ff2ebd36ff0ba4bbb220e1c4b948&units=metric
 
 #[derive(StructOpt, Debug)]
 struct Input {
@@ -50,21 +50,37 @@ impl Weather {
     }
 }
 
+fn print_response(resp: &Weather) {
+    println!(
+        "  当前温度：{}℃ \n  今日最低温：{}℃ \n  今日最高温：{}℃ \n  体感温度：{}℃ \n  湿度：{}% \n  日出时间：{} \n  日落时间：{} \n  当前经度：{} \n  当前纬度：{}",
+        resp.main.temp.to_string().bright_red(),
+        resp.main.temp_min,
+        resp.main.temp_max,
+        resp.main.feels_like,
+        resp.main.humidity,
+        formate_timestamp(resp.sys.sunrise),
+        formate_timestamp(resp.sys.sunset),
+        resp.coord.lon,
+        resp.coord.lat
+    );
+}
+
+fn formate_timestamp(timestamp: i32) -> String {
+    // Creates a new SystemTime from the specified number of whole seconds
+    let time = UNIX_EPOCH + Duration::from_secs(timestamp as u64);
+    // Create DateTime from SystemTime
+    let datetime = DateTime::<Local>::from(time);
+    datetime.format("%H:%M:%S").to_string()
+}
+
 #[tokio::main]
 async fn main() -> Result<(), ExitFailure> {
     let input = Input::from_args();
-    let response = Weather::get(&input.city).await.unwrap();
-    println!(
-        " 当前温度：{}℃ \n 今日最低温：{}℃ \n 今日最高温：{}℃ \n 体感温度：{}℃ \n 湿度：{}% \n 日出时间：{} \n 日落时间：{} \n 当前经度：{} \n 当前纬度：{}",
-        response.main.temp,
-        response.main.temp_min,
-        response.main.temp_max,
-        response.main.feels_like,
-        response.main.humidity,
-        response.sys.sunrise,
-        response.sys.sunset,
-        response.coord.lon,
-        response.coord.lat
-    );
+    println!("{}", "-----正在获取天气信息, 请稍后...-----".bright_green());
+    match Weather::get(&input.city).await {
+        Ok(r) => print_response(&r),
+        Err(e) => println!("请求出错，{:?}", &e),
+    };
+
     Ok(())
 }
