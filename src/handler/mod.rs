132d427193d3401770e1c31e2017b8374ff890b1
JiquanWang99 <951873902@qq.com>
2023-05-20 18:51:43 +0800

    feat: update

--- src/handler/mod.rs
+++ src/handler/mod.rs
@@ -5,17 +5,13 @@ use std::time::{Duration, UNIX_EPOCH};
 use exitfailure::ExitFailure;
 use reqwest::Url;
 use serde_derive::{Deserialize, Serialize};
-use structopt::StructOpt;
 
-#[derive(StructOpt, Debug)]
-pub struct Input {
-    pub city: String,
-}
-
+/// 定义接口返回的数据结构
 #[derive(Deserialize, Serialize, Debug)]
-pub struct Coord {
-    lon: f64,
-    lat: f64,
+pub struct Weather {
+    main: Temperature,
+    sys: Sys,
+    coord: Coord,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -27,23 +23,23 @@ pub struct Temperature {
     humidity: f64,
 }
 
+#[derive(Deserialize, Serialize, Debug)]
+pub struct Coord {
+    lon: f64,
+    lat: f64,
+}
+
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Sys {
     sunrise: i32,
     sunset: i32,
 }
 
-#[derive(Deserialize, Serialize, Debug)]
-pub struct Weather {
-    main: Temperature,
-    sys: Sys,
-    coord: Coord,
-}
-
+/// 定义天气接口的实现
 impl Weather {
     pub async fn get(city: &String) -> Result<Self, ExitFailure> {
         println!("{}", "-----正在获取天气信息, 请稍后...-----".bright_green());
-        let url = format!("http://api.openweathermap.org/data/2.5/weather?q={}&APPID=c6c7ff2ebd36ff0ba4bbb220e1c4b948&units=metric
+        let url = format!("http://api.openweathermap.org/data/2.5/weather?q={}&APPID=c6c7ff2ebd36ff0ba4bbb220e1c4b948
       ", city);
         let url = Url::parse(url.as_str())?;
         let response = reqwest::get(url).await?.json::<Weather>().await?;
@@ -52,15 +48,17 @@ impl Weather {
     }
 }
 
+/// 格式化时间，将时间戳转成指定格式
 pub fn formate_timestamp(timestamp: i32, format: &str) -> String {
     let time = UNIX_EPOCH + Duration::from_secs(timestamp as u64);
     let datetime = DateTime::<Local>::from(time);
     datetime.format(format).to_string()
 }
 
+/// 打印返回结果到控制台
 pub fn print_response(resp: &Weather) {
     println!(
-        "  当前温度：{}℃ \n  今日最低温：{}℃ \n  今日最高温：{}℃ \n  体感温度：{}℃ \n  湿度：{}% \n  日出时间：{} \n  日落时间：{} \n  所在经度：{} \n  所在纬度：{}",
+        "  当前温度：{}℃ \n  当前最低温：{}℃ \n  当前最高温：{}℃ \n  体感温度：{}℃ \n  湿度：{}% \n  今日日出时间：{} \n  今日日落时间：{} \n  所在经度：{} \n  所在纬度：{}",
         resp.main.temp.to_string().bright_red(),
         resp.main.temp_min,
         resp.main.temp_max,
@@ -73,6 +71,7 @@ pub fn print_response(resp: &Weather) {
     );
 }
 
+/// 时间戳的单测
 #[test]
 fn test_timestamp_to_time() {
     assert_eq!(
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 mod handler;
-use handler::{print_response, Input, Weather};
+use handler::{print_response, Weather};
 
 use exitfailure::ExitFailure;
 use structopt::StructOpt;
 
+#[derive(StructOpt, Debug)]
+pub struct Input {
+    pub city: String,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), ExitFailure> {
+    // 获取命令行输入的参数，第一个参数即是 city name
     let input = Input::from_args();
     match Weather::get(&input.city).await {
         Ok(r) => print_response(&r),
